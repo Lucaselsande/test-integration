3e46537008891776ceae19198f1b3571d1933319
lucas sande <lucasgsande55@gmail.com>
2024-06-12 17:59:09 -0300

    respondo 500

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ app.post("/post", async (req, res) => {
   const body = JSON.stringify(req.body)
   console.log('llego evento');
   console.log(body);
-  console.log('respondo 201');
-  res.status(201).send({ message: "OK", body: req.body });
+  console.log('respondo 500');
+  res.status(500).send({ message: "OK", body: req.body });
 });
 app.patch("/comment", async (req, res) => {
   console.log(req.body);
